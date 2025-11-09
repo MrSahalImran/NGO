cdf1de5119743e206b001e80e8d7d7d881edb0b5
sahal <sahalimran.pro@gmail.com>
2025-11-09 15:12:35 +0530

    render config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://ngo-4ux5.onrender.com",
         changeOrigin: true,
         secure: false,
       },
